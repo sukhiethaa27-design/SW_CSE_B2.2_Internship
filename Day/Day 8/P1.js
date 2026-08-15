add75da5133adc94edbf6c179d2271d7b5f1ee3d
sukhiethaa27-design <sukhiethaa27@gmail.com>
2026-08-15 13:57:02 +0530

    JavaScript(JS) usage

---
+++ Day/Day 8/P1.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const mybutton = document.getElementById("external-js");
+
+    mybutton.addEventListener("click", function () {
+        alert("Hello Learnears,Good morning.This is External JS")
+    });
+});
